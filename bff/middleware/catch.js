4d11e2d9d9b18638e6587eccbdd35830c6a118fe
Robin <lotosv2010@163.com>
2025-04-21 14:18:28 +0800

    feat(mq): 实现消息队列

--- bff/middleware/catch.js
+++ bff/middleware/catch.js
@@ -1,6 +1,7 @@
 const { LRUCache } = require('lru-cache');
 const Redis = require('ioredis');
 
+// 创建缓存存储实例
 class CacheStore {
   constructor(options) {
     this.stores = [];
@@ -24,10 +25,11 @@ class CacheStore {
   }
 }
 
+// 内存缓存
 class MemoryStore {
   constructor(options = {
-    max: 100,
-    ttl: 1000 * 60 * 60 * 24
+    max: 100, // 最大缓存数量
+    ttl: 1000 * 60 * 5 // 缓存过期时间，单位为毫秒，设置为 5 分钟，一般来说上层的时间越短
   }) {
     this.cache = new LRUCache(options);
   }
@@ -39,6 +41,7 @@ class MemoryStore {
   }
 }
 
+// Redis 缓存
 class RedisStore {
   constructor(options = {
     host: '127.0.0.1',
@@ -53,17 +56,22 @@ class RedisStore {
     const value = await this.client.get(key);
     return value ? JSON.parse(value) : undefined;
   }
-  async set(key, value, ttl) {
+  async set(key, value) {
     await this.client.set(key, JSON.stringify(value));
+    await this.client.expire(key, 60 * 10); // 缓存过期时间，单位为秒，这里设置为 10 分钟
   }
 }
 
 const cacheMiddleware = (options ={}) => {
   return  async (req, res, next) => {
+    // 创建缓存存储实例
     const cacheStore = new CacheStore();
+    // 添加内存缓存
     cacheStore.add(new MemoryStore());
+    // 添加 Redis 缓存
     const redisStore = new RedisStore(options);
     cacheStore.add(redisStore);
+    // 将缓存存储实例添加到响应对象中
     res.cache = cacheStore;
     await next();
   }
--- bff/src/index.js
+++ bff/src/index.js
@@ -2,12 +2,16 @@ const express = require('express');
 const morgan = require('morgan');
 const rpcMiddleware = require('../middleware/rpc');
 const catchMiddleware = require('../middleware/catch');
+const mqMiddleware = require('../middleware/mq');
+require('./logger'); // 引入日志中间件
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
+
+// 配置 rpc 中间件
 app.use(rpcMiddleware({
   //配置 rpc 中间件的参数，表示要调用的 rpc 接口名称
   interfaceNames: [
@@ -15,38 +19,59 @@ app.use(rpcMiddleware({
     'com.g.bff.post'
   ]
 }))
+
+// 配置 catch 中间件
 app.use(catchMiddleware());
+
+// 配置 mq 中间件
+app.use(mqMiddleware({
+  url: 'amqp://localhost'
+}));
 
 app.get('/', async (req, res) => {
   const { userId } = req.query;
+
+  // 发送日志到 RabbitMQ
+  res.channels.logger.sendToQueue('logger', Buffer.from(JSON.stringify({
+    method: req.method,
+    path: req.path,
+    userId
+  })));
+
   const { user, post } = res?.rpcConsumers || {};
 
+  // 缓存数据
   const cacheKey = `${req.method}-${req.path}-${userId}`;
   let cacheData = await res.cache.get(cacheKey);
-  console.log('cacheData', cacheData);
   if (cacheData) {
     return res.json(cacheData);
   }
 
+  // 调用 rpc 接口
   const [userInfo, postList] = await Promise.all([
     user.invoke('getUserInfo', [userId]),
     post.invoke('getPostList', [userId])
   ]);
-  // 裁剪数据
-  Reflect.deleteProperty(userInfo, 'password');
-  // 数据脱敏
-  Reflect.set(userInfo, 'phone', userInfo.phone.replace(/(\d{3})\d{4}(\d{4})/, '$1****$2'));
-  // 数据适配
-  // userInfo.avatar = "http://www.zhufengpeixun.cn/" + userInfo.avatar;
+
+  // 数据处理
+  if(userInfo) {
+    // 裁剪数据
+    Reflect.deleteProperty(userInfo, 'password');
+    // 数据脱敏
+    Reflect.set(userInfo, 'phone', userInfo.phone.replace(/(\d{3})\d{4}(\d{4})/, '$1****$2'));
+    // 数据适配
+    // userInfo.avatar = "http://www.zhufengpeixun.cn/" + userInfo.avatar;
+  }
+
+  // 缓存数据
   cacheData = {
     userInfo,
     postList
   };
   await res.cache.set(cacheKey, cacheData);
-  res.json({
-    userInfo,
-    postList
-  });
+
+  // 返回数据
+  res.json(cacheData);
 });
 
 app.listen(3300, () => {
